1f4c896c9e2c7e4c736df503d8f7c83cbff4d72e
jack grime <jackgrime@mugstudios.co.uk>
2023-07-01 22:09:03 +0100

    added drifting weighting and record timing of drift

--- Assets/Scripts/SimpleCarController.cs
+++ Assets/Scripts/SimpleCarController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:109a5fb291e4ab0617681db02bdfff884875af637c74dcfc52a806d20e9b3600
-size 13430
+oid sha256:ea8e06fcc57337840ce06a869c5f936e8a4b7bb7d4b1704c98a8c8d0506a9476
+size 14137
