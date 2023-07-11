bb7ad6002bf41db851bf30607282756b9100af6d
jack grime <jackgrime@mugstudios.co.uk>
2023-07-11 22:48:20 +0100

    work on Iaspect

---
+++ Assets/Scenes/SecondAttempt/NetCode Components/MoveToPositionAspect.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9d0cc39601c00f6660ac64c2587449c8bd248c0319dbce62a9c24541b58c2417
+size 759
--- Assets/Scenes/SecondAttempt/System/MovingSystemBase.cs
+++ Assets/Scenes/SecondAttempt/System/MovingSystemBase.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:269f6e185f9108ca818e19e587a9ef08d65fa9ade4de2a372ba0ebd3b2802846
-size 906
+oid sha256:8823e0b2ccbd7b8c7c17b704af59180f5172d41c6803b3449bb58531ae9582f5
+size 745
